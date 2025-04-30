ce3bf4605a62a87ba24519d6a403391c859dbecb
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2025-04-30 03:58:45 +0300

    initial setup of the package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BudgetFeature",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BudgetFeature",
+            targets: ["BudgetFeature"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "BudgetFeature"),
+        .testTarget(
+            name: "BudgetFeatureTests",
+            dependencies: ["BudgetFeature"]
+        ),
+    ]
+)
